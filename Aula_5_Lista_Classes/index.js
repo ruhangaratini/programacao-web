1c97e3db953b758bee8e4469687e814d39abcd58
ruhangaratini <garatini.ruhan@gmail.com>
2024-03-08 19:53:31 -0300

    :white_check_mark: Finalização da lista 02

--- Aula_5_Lista_Classes/index.js
+++ Aula_5_Lista_Classes/index.js
@@ -79,6 +79,124 @@ var Calculator = /** @class */ (function () {
         this.value1 = value1;
         this.value2 = value2;
     }
+    Object.defineProperty(Calculator.prototype, "getValue1", {
+        get: function () {
+            return this.value1;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Calculator.prototype, "setValue1", {
+        set: function (value) {
+            this.value1 = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Calculator.prototype, "getValue2", {
+        get: function () {
+            return this.value2;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Calculator.prototype, "setValue2", {
+        set: function (value) {
+            this.value2 = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Calculator.prototype.sum = function () {
+        return this.value1 + this.value2;
+    };
+    Calculator.prototype.subtract = function () {
+        return this.value1 - this.value2;
+    };
+    Calculator.prototype.multiply = function () {
+        return this.value1 * this.value2;
+    };
+    Calculator.prototype.divide = function () {
+        if (this.value2 == 0)
+            throw new Error("Não é possível dividir por zero");
+        return this.value1 / this.value2;
+    };
+    Calculator.prototype.percentage = function () {
+        return this.value1 / 100 * this.value2;
+    };
     return Calculator;
 }());
-var myCalculator = new Calculator(1, 1);
+var myCalculator = new Calculator(20, 10);
+console.log('Soma: ', myCalculator.sum());
+console.log('Subtração: ', myCalculator.subtract());
+console.log('Multiplicação: ', myCalculator.multiply());
+console.log('Porcentagem: ', myCalculator.percentage());
+console.log('Divisão: ', myCalculator.divide());
+myCalculator.setValue2 = 0;
+// console.log('Divisão por zero: ', myCalculator.divide());
+var Product = /** @class */ (function () {
+    function Product(name, price, stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+    Object.defineProperty(Product.prototype, "getName", {
+        get: function () {
+            return this.name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Product.prototype, "setName", {
+        set: function (name) {
+            this.name = name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Product.prototype, "getPrice", {
+        get: function () {
+            return this.price;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Product.prototype, "setPrice", {
+        set: function (price) {
+            this.price = price;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Product.prototype, "getStock", {
+        get: function () {
+            return this.stock;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Product.prototype, "setStock", {
+        set: function (stock) {
+            this.stock = stock;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Product.prototype.totalValueInStock = function () {
+        return this.price * this.stock;
+    };
+    Product.prototype.addStock = function (quantity) {
+        this.stock += quantity;
+    };
+    Product.prototype.sale = function (quantity) {
+        if (this.stock - quantity < 0)
+            throw new Error('Estoque insuficiente');
+        this.stock -= quantity;
+    };
+    return Product;
+}());
+var mouse = new Product("Mouse Brastemp", 56.89, 10);
+console.log('Valor total em estoque: ', mouse.totalValueInStock());
+mouse.addStock(5);
+mouse.sale(3);
+console.log("estoque: ", mouse.getStock);
--- Aula_5_Lista_Classes/index.ts
+++ Aula_5_Lista_Classes/index.ts
@@ -60,13 +60,116 @@ class Car {
   console.log(myNewCar.age());
   
 class Calculator {
-  value1:number;
-  value2:number;
+  private value1:number;
+  private value2:number;
 
   constructor(value1:number, value2:number) {
     this.value1 = value1;
     this.value2 = value2;
   }
+
+  get getValue1() : number {
+    return this.value1;
+  }
+
+  set setValue1(value:number) {
+    this.value1 = value;
+  }
+
+  get getValue2() : number {
+    return this.value2;
+  }
+
+  set setValue2(value:number) {
+    this.value2 = value;
+  }
+
+  sum(): number {
+    return this.value1 + this.value2;
+  }
+
+  subtract() : number {
+    return this.value1 - this.value2;
+  }
+
+  multiply(): number {
+    return this.value1 * this.value2;
+  }
+
+  divide() : number {
+    if(this.value2 == 0) throw new Error("Não é possível dividir por zero");
+    return this.value1 / this.value2;
+  }
+
+  percentage() : number {
+    return this.value1/100 * this.value2;
+  }
+
 }
 
-const myCalculator = new Calculator(1, 1);
+const myCalculator = new Calculator(20, 10);
+console.log('Soma: ', myCalculator.sum());
+console.log('Subtração: ', myCalculator.subtract());
+console.log('Multiplicação: ', myCalculator.multiply());
+console.log('Porcentagem: ', myCalculator.percentage());
+console.log('Divisão: ', myCalculator.divide());
+
+myCalculator.setValue2 = 0;
+
+// console.log('Divisão por zero: ', myCalculator.divide());
+
+class Product {
+  name:string;
+  price:number;
+  stock:number;
+  
+  constructor (name:string, price:number, stock:number) {
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  get getName() : string {
+    return this.name;
+  }
+
+  set setName(name:string) {
+    this.name = name;
+  }
+
+  get getPrice() : number {
+    return this.price;
+  }
+
+  set setPrice(price:number) {
+    this.price = price;
+  }
+
+  get getStock() : number {
+    return this.stock;
+  }
+
+  set setStock(stock:number) {
+    this.stock = stock;
+  }
+
+  totalValueInStock() : number {
+    return this.price * this.stock;
+  }
+
+  addStock(quantity:number) {
+    this.stock += quantity;
+  }
+
+  sale(quantity:number) {
+    if(this.stock - quantity < 0)
+      throw new Error('Estoque insuficiente');
+    this.stock -= quantity;
+  }
+}
+
+let mouse = new Product("Mouse Brastemp", 56.89, 10);
+console.log('Valor total em estoque: ', mouse.totalValueInStock());
+mouse.addStock(5);
+mouse.sale(3);
+console.log("estoque: ", mouse.getStock);
